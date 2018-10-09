d9d17cba70955578dbfc65f85a8d614141e8e50b
AshBanzai <nicolas18char@gmail.com>
2018-10-09 15:51:49 +0200

    Ajout connexion

--- PPE3_osu_Github/FConnexion.Designer.cs
+++ PPE3_osu_Github/FConnexion.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PPE3_osu_Github
 {
-    partial class Form1
+    partial class FConnexion
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,14 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FConnexion));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dgvTest = new System.Windows.Forms.DataGridView();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.bsTest = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsTest)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -46,6 +52,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Quitter";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -55,6 +62,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "OK";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -65,20 +73,20 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Identifiez vous";
             // 
-            // textBox1
+            // txtIdentifiant
             // 
-            this.textBox1.Location = new System.Drawing.Point(233, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtIdentifiant.Location = new System.Drawing.Point(233, 55);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(100, 20);
+            this.txtIdentifiant.TabIndex = 3;
             // 
-            // textBox2
+            // txtPasswd
             // 
-            this.textBox2.Location = new System.Drawing.Point(233, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.txtPasswd.Location = new System.Drawing.Point(233, 94);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.PasswordChar = '*';
+            this.txtPasswd.Size = new System.Drawing.Size(100, 20);
+            this.txtPasswd.TabIndex = 4;
             // 
             // pictureBox1
             // 
@@ -90,20 +98,42 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
-            // Form1
+            // dgvTest
+            // 
+            this.dgvTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTest.Location = new System.Drawing.Point(424, 82);
+            this.dgvTest.Name = "dgvTest";
+            this.dgvTest.Size = new System.Drawing.Size(240, 150);
+            this.dgvTest.TabIndex = 6;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(509, 238);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 7;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // FConnexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.dgvTest);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.txtIdentifiant);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
+            this.Name = "FConnexion";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsTest)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,9 +144,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.TextBox txtPasswd;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dgvTest;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.BindingSource bsTest;
     }
 }
 
--- PPE3_osu_Github/Program.cs
+++ PPE3_osu_Github/Program.cs
@@ -18,7 +18,8 @@ namespace PPE3_osu_Github
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Modele.init();
+            Application.Run(new FConnexion());
         }
     }
 }
